c857d3123b5eb5f6cf3a0fb63f572249e3a219e1
LautaroCepeda-Developer <lautarocepeda.developer@gmail.com>
2025-06-02 10:03:36 -0300

    Feat: Finished user schema

--- src/schemas/user.mjs
+++ src/schemas/user.mjs
@@ -1,11 +1,41 @@
 import * as z from 'zod';
+import { isUsernameValid } from '../tools/commonValidations.mjs';
 
 const userSchema = z.object({
     id: z
         .number({
-            invalid_type_error: 'Id must be a integer',
-            required_error: 'Id is required.',
+            invalid_type_error: 'Id must be a integer.',
+            required_error: 'id is required.',
+        })
+        .int()
+        .min(1),
+    fullname: z
+        .string({
+            required_error: 'fullname is required.',
+        })
+        .min(3),
+    email: z
+        .string({
+            required_error: 'email is required.',
+        })
+        .toLowerCase()
+        .email({ pattern: z.regexes.unicodeEmail }),
+    username: z
+        .string({ required_error: 'username is required.' })
+        .min(5)
+        .refine((val) => isUsernameValid(val), {
+            message: 'username contains invalid characters.',
+        }),
+    password: z.string({
+        required_error: 'password is required.',
+    }),
+    roleId: z
+        .number({
+            invalid_type_error: 'roleId must be a integer',
+            required_error: 'roleId is required.',
         })
         .int()
         .min(1),
 });
+
+const roleSchema = z.object({});
