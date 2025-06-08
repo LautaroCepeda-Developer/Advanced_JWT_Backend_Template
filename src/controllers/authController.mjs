cfb558067cadf646ced9a36cbd7c54dea25de4de
LautaroCepeda-Developer <lautarocepeda.developer@gmail.com>
2025-06-08 14:18:13 -0300

    FIX: Incorrect status code in auth routes

--- src/controllers/authController.mjs
+++ src/controllers/authController.mjs
@@ -6,18 +6,14 @@ export const register = async (req, res) => {
     const { username, password } = req.body;
 
     if (!username || !password) {
-        return res
-            .status(res.statusCode || 400)
-            .json({ message: missingFieldsMessage });
+        return res.status(400).json({ message: missingFieldsMessage });
     }
 
     try {
         await AuthService.registerUser(req, res);
         return res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
-        return res
-            .status(res.statusCode || 400)
-            .json({ message: error.message });
+        return res.status(400).json({ message: error.message });
     }
 };
 
@@ -25,14 +21,12 @@ export const login = async (req, res) => {
     const { username, password } = req.body;
 
     if (!username || !password) {
-        return res
-            .status(res.statusCode || 400)
-            .json({ message: missingFieldsMessage });
+        return res.status(400).json({ message: missingFieldsMessage });
     }
 
     try {
         const token = await AuthService.loginUser(req, res);
-        return res.status(res.statusCode || 400).json({ token });
+        return res.status(200).json({ token });
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
